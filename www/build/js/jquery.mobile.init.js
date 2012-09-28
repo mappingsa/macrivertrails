0c563eccd44e52c2c35276910cc14e061a69e9d2
Jon Tara <jtara-github-public@spamex.com>
2012-09-27 17:06:41 -0700

    Set default transition to slide

--- www/build/js/jquery.mobile.init.js
+++ www/build/js/jquery.mobile.init.js
@@ -2,9 +2,12 @@
 // This code MUST be included AFTER jquery is loaded, and BEFORE jQuery Mobile is loaded!
 $(document).bind("mobileinit", function(){
     //$.mobile.touchOverflowEnabled = true;  // Deprecated in 1.1
+    // Pushstate should be enabled for website, as otherwise user will see confusing URLs in
+    // URL bar. For PhoneGap, it's better to disable pushstate
     $.mobile.pushStateEnabled = true;
     // TODO: This doesn't seem to actually work and is undocumented. It is, however, present
     // in the jQuery Mobile code. Research how to actually disable orientation change, most
     // likely with an event handler.
     $.mobile.orientationChangeEnabled = false;
+    $.mobile.defaultTransition = "slide";
 });
