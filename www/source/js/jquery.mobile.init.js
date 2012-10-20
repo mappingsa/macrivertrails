adea96442ffccf2c7dd048af070d6cc16baddd82
Jon Tara <jtara-github-public@spamex.com>
2012-10-20 12:16:54 -0700

    Add some browser tests. Only apply CSS hardware acceleration hints for real Apple WebKit.

--- www/source/js/jquery.mobile.init.js
+++ www/source/js/jquery.mobile.init.js
@@ -1,6 +1,14 @@
 // Initialize jQuery Mobile options.
 // This code MUST be included AFTER jquery is loaded, and BEFORE jQuery Mobile is loaded!
 $(document).bind("mobileinit", function(){
+    // This is an imperfect test for a genuine Apple Webkit. Chrome spoofs this, so we exclude
+    //   Chrome. But what else is out there that spoofs Apple Webkit?
+    var version = navigator.appVersion.toLowerCase(),
+        isAppleWebkit =  (/.*applewebkit.(?!.*chrome)/).test(version),
+        isAppleWebkitMobile = isAppleWebkit && (/.*mobile/).test(version), // true if Apple Webkit not on desktop
+        isAppleWebkitDesktop = isAppleWebkit && !isAppleWebkitMobile,
+        hintAcceleration = isAppleWebkit,     // true if should hint hardware acceleration
+        $html = $("html");
     //$.mobile.touchOverflowEnabled = true;  // Deprecated in 1.1
     // Pushstate should be enabled for website, as otherwise user will see confusing URLs in
     // URL bar. For PhoneGap, it's better to disable pushstate
@@ -10,4 +18,18 @@ $(document).bind("mobileinit", function(){
     // likely with an event handler.
     $.mobile.orientationChangeEnabled = false;
     $.mobile.defaultPageTransition = "slide";
+
+    // Add some classes to HTML so that we can write CSS that is responsive to browser model
+    if (isAppleWebkit) {
+      $html.addClass("is-apple-webkit");
+      // Make this a separate class, in case other platforms might need this as well
+      // Currently, I don't know of any, but it seems prudent to keep this separate
+      $html.addClass("hint-acceleration");
+    }
+    if (isAppleWebkitMobile) {
+      $html.addClass("is-apple-webkit-mobile");
+    }
+    if (isAppleWebkitDesktop) {
+      $html.addClass("is-apple-webkit-desktop")
+    }
 });
