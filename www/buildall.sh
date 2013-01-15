fc93988973b92795d93d57c13edab03cc6403a78
Jon Tara <jtara-github-public@spamex.com>
2013-01-14 19:11:26 -0800

    Issue #119 Add separate builds for phonegap-ios and phonegap-android

--- www/buildall.sh
+++ www/buildall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 MIDDLEMAN_BUILD_TARGET=debug middleman build
 MIDDLEMAN_BUILD_TARGET=web middleman build
-MIDDLEMAN_BUILD_TARGET=phonegap middleman build
+MIDDLEMAN_BUILD_TARGET=phonegap-ios middleman build
+MIDDLEMAN_BUILD_TARGET=phonegap-android middleman build
