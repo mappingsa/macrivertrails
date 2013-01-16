1d4befcbb699c92ddb31416a54d5129f2dcd41f6
Jon Tara <jtara-github-public@spamex.com>
2013-01-15 20:08:05 -0800

    Issue #122 Added a phonegap-ios-debug Middleman build, so that Web Inspector debugging can be done when running in PhoneGap

--- www/buildall.sh
+++ www/buildall.sh
@@ -2,4 +2,5 @@
 MIDDLEMAN_BUILD_TARGET=debug middleman build
 MIDDLEMAN_BUILD_TARGET=web middleman build
 MIDDLEMAN_BUILD_TARGET=phonegap-ios middleman build
+MIDDLEMAN_BUILD_TARGET=phonegap-ios-debug middleman build
 MIDDLEMAN_BUILD_TARGET=phonegap-android middleman build
