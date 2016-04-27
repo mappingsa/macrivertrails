8686a6e04d43a17d32ccfddd7dd42e34ea0d6037
Jon Tara <jtara-github-public@spamex.com>
2016-04-26 23:01:43 -0700

    Fixes to config.rb (version number updates)

--- www/config.rb
+++ www/config.rb
@@ -29,18 +29,16 @@ configure :build do
   if build_target =~ /debug/
     # This is a debug build, and we are not minifying JS/CSS
     # Do not include vendor pre-minified files in the build.
-    ignore '/js/jquery.mobile-1.2.0.min.js'
-    ignore '/js/jquery-1.7.2.min.js'
+    ignore '/js/jquery.mobile-1.2.1.min.js'
     ignore '/js/jquery-1.8.2.min.js'
-    ignore '/css/jquery.mobile.structure-1.2.0.min.css'
+    ignore '/css/jquery.mobile.structure-1.2.1.min.css'
   else
     activate :minify_css
     activate :minify_javascript
     # This is a production build.
     # The following files are pre-minified by the vendor. Do not include the
     # uncompressed versions in the build.
-    ignore '/js/jquery.mobile-1.2.0.js'
-    ignore '/js/jquery-1.7.2.js'
+    ignore '/js/jquery.mobile-1.2.1.js'
     ignore '/js/jquery-1.8.2.js'
     ignore '/css/jquery.mobile.structure-1.2.0.css'
   end
